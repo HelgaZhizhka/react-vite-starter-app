455f0d81f664442e2c91efe871df6caf2c30eed1
HelgaZhizhka <helgazhyzhka@gmail.com>
2025-02-07 19:30:47 +0100

    structure project

---
+++ src/core/error-boundary/error-boundary.tsx
@@ -0,0 +1,38 @@
+import React, { ReactNode } from 'react';
+import type { ErrorInfo } from 'react';
+
+interface ErrorBoundaryProps {
+  children: ReactNode;
+}
+
+interface ErrorBoundaryState {
+  hasError: boolean;
+}
+
+class ErrorBoundary extends React.Component<
+  ErrorBoundaryProps,
+  ErrorBoundaryState
+> {
+  constructor(props: ErrorBoundaryProps) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError(error: Error): ErrorBoundaryState {
+    console.error('Error!', error);
+    return { hasError: true };
+  }
+
+  componentDidCatch(error: Error, info: ErrorInfo): void {
+    console.error('Error!', error, info);
+  }
+
+  render(): React.ReactNode {
+    if (this.state.hasError) {
+      return <h2>Ups something wrong</h2>;
+    }
+    return this.props.children;
+  }
+}
+
+export default ErrorBoundary;
