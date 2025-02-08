aaba2296ad1882434c444af664290f0bf7efb7d2
HelgaZhizhka <helgazhyzhka@gmail.com>
2025-02-08 21:18:59 +0100

    fix error boundary class

--- src/core/error-boundary/error-boundary.tsx
+++ src/core/error-boundary/error-boundary.tsx
@@ -1,4 +1,4 @@
-import React, { ReactNode } from 'react';
+import { Component, ReactNode } from 'react';
 import type { ErrorInfo } from 'react';
 
 interface ErrorBoundaryProps {
@@ -9,27 +9,23 @@ interface ErrorBoundaryState {
   hasError: boolean;
 }
 
-class ErrorBoundary extends React.Component<
-  ErrorBoundaryProps,
-  ErrorBoundaryState
-> {
+class ErrorBoundary extends Component<ErrorBoundaryProps, ErrorBoundaryState> {
   constructor(props: ErrorBoundaryProps) {
     super(props);
     this.state = { hasError: false };
   }
 
-  static getDerivedStateFromError(error: Error): ErrorBoundaryState {
-    console.error('Error!', error);
+  static getDerivedStateFromError(): ErrorBoundaryState {
     return { hasError: true };
   }
 
   componentDidCatch(error: Error, info: ErrorInfo): void {
-    console.error('Error!', error, info);
+    console.error('Caught by ErrorBoundary:', error, info);
   }
 
   render(): React.ReactNode {
     if (this.state.hasError) {
-      return <h2>Ups something wrong</h2>;
+      return <h2>Ups! Something went wrong 😢</h2>;
     }
     return this.props.children;
   }
